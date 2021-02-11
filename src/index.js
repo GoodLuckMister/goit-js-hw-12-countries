6853345000023d320699d576ffa0450bf84e4e47
GodLuckMister <paet14@gmail.com>
2021-02-11 02:09:47 +0200

    add template and fetch in function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import './styles.css';
+import countryCardTmp from './templates/country.hbs';
+import debounce from 'lodash.debounce';
 
-fetch('https://restcountries.eu/rest/v2/name/ukraine')
-  .then(response => response.json())
-  .then(console.log);
+const refs = {
+  cardContainer: document.querySelector('.js-card__container'),
+  inputRef: document.querySelector('.js__input'),
+};
+
+refs.inputRef.addEventListener('input', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  const inputText = refs.inputRef.value;
+
+  fetchCountryByName(inputText)
+    .then(renderCountryCard)
+    .catch(error => console.log(error));
+}
+
+function fetchCountryByName(countryName) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(
+    response => {
+      return response.json();
+    },
+  );
+}
+
+function renderCountryCard(country) {
+  const markup = countryCardTmp(country);
+  refs.cardContainer.innerHTML = markup;
+}
